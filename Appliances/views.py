8a15098d11f22dc8894e9630dbf2359c251d731e
Dimitrii <ikzdimitrij@gmail.com>
2017-05-19 15:03:11 +0300

    bugfix

--- Appliances/views.py
+++ Appliances/views.py
@@ -6,4 +6,4 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index!")
