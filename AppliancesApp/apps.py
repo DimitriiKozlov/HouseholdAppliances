001f81cf2827ea37d28fbf3642574285a0612a70
Dimitrii <ikzdimitrij@gmail.com>
2017-05-28 15:18:03 +0300

    add Question

--- AppliancesApp/apps.py
+++ AppliancesApp/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class AppliancesConfig(AppConfig):
-    name = 'Appliances'
+    name = 'AppliancesApp'
--- AppliancesSite/settings.py
+++ AppliancesSite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Appliances.apps.AppliancesConfig',
+    'AppliancesApp.apps.AppliancesConfig',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Kiev'
 
 USE_I18N = True
 
